4949d187a8fb0e8cd90145b5440b480e413ed2c2
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-24 18:22:19 -0600

    Create sad path for cities with no weather info

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,4 +2,8 @@ class Destination < ApplicationRecord
   def weather
     Weather.new(name)
   end
+
+  def has_weather?
+    Weather.new
+  end
 end
--- app/poros/weather.rb
+++ app/poros/weather.rb
@@ -1,12 +1,41 @@
 class Weather
-  attr_reader :date, :current, :high, :low, :summary
+  # attr_reader :date, :current, :high, :low, :summary
+  # def initialize(destination)
+  #   weather_info = get_weather(destination)
+  #   @date = Date.today
+  #   @current = weather_info[:main][:temp]
+  #   @high = weather_info[:main][:temp_max]
+  #   @low = weather_info[:main][:temp_min]
+  #   @summary = weather_info[:weather].first[:description]
+  # end
+
   def initialize(destination)
-    weather_info = get_weather(destination)
-    @date = Date.today
-    @current = weather_info[:main][:temp]
-    @high = weather_info[:main][:temp_max]
-    @low = weather_info[:main][:temp_min]
-    @summary = weather_info[:weather].first[:description]
+    @weather_info = get_weather(destination)
+  end
+
+  def date
+    Date.today if !@weather_info[:message] = 'city not found'
+    "unavailable"
+  end
+
+  def current
+    weather_info[:main][:temp] if !@weather_info[:message] = 'city not found'
+    "unavailable"
+  end
+
+  def high
+    weather_info[:main][:temp_max] if !@weather_info[:message] = 'city not found'
+    'unavailable'
+  end
+
+  def low
+    weather_info[:main][:temp_min] if !@weather_info[:message] = 'city not found'
+    'unavailable'
+  end
+
+  def summary
+    weather_info[:weather].first[:description] if !@weather_info[:message] = 'city not found'
+    'unavailable'
   end
 
   def get_weather(destination)
