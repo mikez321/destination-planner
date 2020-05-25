89361d307ffe1ff3fb22ca42cd0e6929e18c682b
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-24 21:40:51 -0600

    Create sad path testing for not finding weather

--- spec/features/current_weather_spec.rb
+++ spec/features/current_weather_spec.rb
@@ -25,8 +25,36 @@ describe "it knows the weather" do
       expect(page).to have_css(".high")
       expect(page).to have_css(".low")
       expect(page).to have_css(".summary")
+      expect(page).to_not have_content 'unavailable'
+    end
+  end
+
+  it 'will fail gracefully if a place does not have weather' do
+    not_a_place = Destination.create(name: 'Fake place',
+                                    zip: '12345',
+                                    description: 'This place is not real',
+                                    image_url: Faker::Placeholdit.image)
+
+    visit root_path
+
+    within ".destination-#{not_a_place.id}" do
+      click_link "Show"
     end
 
+    expect(current_path).to eq destination_path(not_a_place.id)
+
+    expect(page).to have_content not_a_place.name
+    expect(page).to have_content not_a_place.zip
+    expect(page).to have_content not_a_place.description
+
+    within ".weather" do
+      expect(page).to have_css(".date")
+      expect(page).to have_css(".current")
+      expect(page).to have_css(".high")
+      expect(page).to have_css(".low")
+      expect(page).to have_css(".summary")
+      expect(page).to have_content('unavailable', count: 5)
+    end
   end
 end
 # As a user
