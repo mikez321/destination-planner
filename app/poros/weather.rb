d170266905c74625de95cef05b1c67cdeddbc661
Mike Hernandez <hernandezmichael321@gmail.com>
2020-05-24 21:35:01 -0600

    Update weather PORO to inlcude images

--- app/poros/weather.rb
+++ app/poros/weather.rb
@@ -1,48 +1,63 @@
 class Weather
-  # attr_reader :date, :current, :high, :low, :summary
-  # def initialize(destination)
-  #   weather_info = get_weather(destination)
-  #   @date = Date.today
-  #   @current = weather_info[:main][:temp]
-  #   @high = weather_info[:main][:temp_max]
-  #   @low = weather_info[:main][:temp_min]
-  #   @summary = weather_info[:weather].first[:description]
-  # end
-
   def initialize(destination)
     @weather_info = get_weather(destination)
   end
 
   def date
-    Date.today if !@weather_info[:message] = 'city not found'
-    "unavailable"
+    if @weather_info[:message].nil?
+      Date.today
+    else
+      "unavailable"
+    end
   end
 
   def current
-    weather_info[:main][:temp] if !@weather_info[:message] = 'city not found'
-    "unavailable"
+    if @weather_info[:message].nil?
+      @weather_info[:main][:temp]
+    else
+      'unavailable'
+    end
   end
 
   def high
-    weather_info[:main][:temp_max] if !@weather_info[:message] = 'city not found'
-    'unavailable'
+    if @weather_info[:message].nil?
+      @weather_info[:main][:temp_max]
+    else
+      'unavailable'
+    end
   end
 
   def low
-    weather_info[:main][:temp_min] if !@weather_info[:message] = 'city not found'
-    'unavailable'
+    if @weather_info[:message].nil?
+      @weather_info[:main][:temp_min]
+    else
+      'unavailable'
+    end
   end
 
   def summary
-    weather_info[:weather].first[:description] if !@weather_info[:message] = 'city not found'
-    'unavailable'
+    if @weather_info[:message].nil?
+      @weather_info[:weather].first[:description]
+    else
+      'unavailable'
+    end
   end
 
   def image
-    'https://ctl.s6img.com/society6/img/E54QZiyFd-Fu7kFpNdgXFnKycJc/w_700/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/eb37338848f64aefb6b83b59757c2457/~~/good-morning-sunshine1225559-prints.jpg'
+    if @weather_info[:meassage].nil?
+      get_image(@weather_info[:weather].first[:main])
+    else
+      'unavailable'
+    end
   end
+
+  private
 
   def get_weather(destination)
     WeatherService.new.current_weather(destination)
+  end
+
+  def get_image(keyword)
+    GiphyService.new.weather_image(keyword)
   end
 end
